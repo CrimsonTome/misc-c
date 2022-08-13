545f1abef3b0ad3add826b218516a28ba5eb6e6e
CrimsonTome <crimsontome427@protonmail.com>
2022-08-13 12:20:51 +0100

    printing variables

--- test.c
+++ test.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
 int main() {
-  printf("Hello World!");
-  return 0;
+    printf("Hello World!");
+
+    int x = 5;
+    int y = 6;
+    int sum = x + y;
+    printf("\nThe sum of %i and %i is %i\n", x, y, sum);
+    return 0;
 } 
